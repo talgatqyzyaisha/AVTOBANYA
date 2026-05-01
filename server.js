6c70763a80895724564503a942084123857bb3d1
Aisha <aishatalgatqyzy5129@gmail.com>
2026-05-01 19:48:34 +0500

    Fix

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 });
 
 // 4. Обработка остальных путей (чтобы не было Cannot GET)
-app.get('/:any(.*)', (req, res) => {
+app.get('/:path*', (req, res) => {
   if (!req.path.startsWith('/api')) {
     res.sendFile(path.join(root, 'frontend/pages/index.html'));
   }
